7d4255ca576702e25ef1f3f278498a913bac8d1e
divyamgautam <divyamgautam15@hotmail.com>
2022-03-25 23:17:50 +0530

    Completed the Happy Places RecyclerView with update feature after adding new Happy Places

--- app/src/main/java/com/example/happyplacesapp/activities/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/example/happyplacesapp/activities/AddHappyPlaceActivity.kt
@@ -109,7 +109,7 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener{
                          val dbHandler = DatabaseHandler(this)
                          val addHappyPlace = dbHandler.addHappyPlace(happyPlaceModel)
                          if (addHappyPlace > 0){
-                             Toast.makeText(this, "Happy Place Added!!", Toast.LENGTH_SHORT).show()
+                             setResult(Activity.RESULT_OK)
                              finish()
                          }
                   }
--- app/src/main/java/com/example/happyplacesapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/happyplacesapp/activities/MainActivity.kt
@@ -3,6 +3,7 @@
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.happyplacesapp.activities.AddHappyPlaceActivity
@@ -10,7 +11,6 @@ import com.example.happyplacesapp.adapters.HappyPlacesAdapter
 import com.example.happyplacesapp.database.DatabaseHandler
 import com.example.happyplacesapp.databinding.ActivityMainBinding
 import com.example.happyplacesapp.models.HappyPlaceModel
-
    class MainActivity : AppCompatActivity() {
 
        private var binding: ActivityMainBinding? = null
@@ -21,7 +21,7 @@ import com.example.happyplacesapp.models.HappyPlaceModel
             setContentView(binding?.root)
             binding?.fabAddHappyPlace?.setOnClickListener {
                 val intent = Intent(this, AddHappyPlaceActivity::class.java)
-                startActivity(intent)
+                startActivityForResult(intent, ADD_HAPPY_PLACE_REQUEST_CODE)
             }
             getHappyPlacesFromLocalDB()
         }
@@ -33,6 +33,7 @@ import com.example.happyplacesapp.models.HappyPlaceModel
             binding?.rvHappyPlaces?.adapter = placesAdapter
 
         }
+
         private fun getHappyPlacesFromLocalDB(){
             val dbHandler = DatabaseHandler(this)
             val getHappyPlaceList: ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList()
@@ -44,5 +45,18 @@ import com.example.happyplacesapp.models.HappyPlaceModel
                 binding?.rvHappyPlaces?.visibility = View.GONE
                 binding?.tvNoRecords?.visibility = View.VISIBLE
             }
+        }
+       override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+           super.onActivityResult(requestCode, resultCode, data)
+           if(requestCode == ADD_HAPPY_PLACE_REQUEST_CODE){
+               if (resultCode == RESULT_OK){
+                   getHappyPlacesFromLocalDB()
+               }else{
+                   Log.e("Activity", "Cancelled or Back Pressed")
+               }
+           }
+       }
+        companion object{
+            var ADD_HAPPY_PLACE_REQUEST_CODE = 1
         }
 }
